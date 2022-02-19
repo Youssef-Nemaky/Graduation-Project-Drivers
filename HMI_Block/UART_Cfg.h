96e55d9b301bc10734217b132450143d16cf4aa7
Youssef El-Nemaky <youssef.mmdoh99@gmail.com>
2022-02-19 16:45:26 +0200

    Add uart init code and all required files

---
+++ HMI_Block/UART_Cfg.h
@@ -0,0 +1,27 @@
+/*
+ * UART_Cfg.h
+ *
+ *  Created on: Feb 3, 2022
+ *      Author: dell
+ */
+
+
+
+#ifndef UART_CFG_H
+#define UART_CFG_H
+
+/* A configuration to the number of UART groups*/
+#define UART_CONFIGURED_NUMBER             (2U)
+
+#define UART0_ENABLE                       (STD_ON)
+#define UART1_ENABLE                       (STD_ON)
+#define UART2_ENABLE                       (STD_OFF)
+#define UART3_ENABLE                       (STD_OFF)
+#define UART4_ENABLE                       (STD_OFF)
+#define UART5_ENABLE                       (STD_OFF)
+#define UART6_ENABLE                       (STD_OFF)
+#define UART7_ENABLE                       (STD_OFF)
+
+
+
+#endif /* UART_CFG_H_ */
